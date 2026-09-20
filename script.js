b9ecc0d3b27374b3ee6053b05df2c7399ff074ce
Atakan Yalçın <atakanyaln@gmail.com>
2026-09-20 18:58:31 +0300

    Computer can now play

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+function getComputerChoice() {
+    let result
+    switch(Math.floor(Math.random() * 3)) {
+        case 0:
+            result = "rock"
+            break
+        case 1:
+            result = "paper"
+            break
+        case 2:
+            result = "scissors"
+            break
+    }
+    return result
+}
+choice = getComputerChoice()
+console.log(choice)
