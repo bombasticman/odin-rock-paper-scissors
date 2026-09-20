367a263c3624d4617a89240f30e3d8402d47fe99
Atakan Yalçın <atakanyaln@gmail.com>
2026-09-20 19:11:12 +0300

    Human choice function complete

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 function getComputerChoice() {
-    let result
+    let choice
     switch(Math.floor(Math.random() * 3)) {
         case 0:
-            result = "rock"
+            choice = "rock"
             break
         case 1:
-            result = "paper"
+            choice = "paper"
             break
         case 2:
-            result = "scissors"
+            choice = "scissors"
             break
     }
-    return result
+    return choice
 }
-choice = getComputerChoice()
-console.log(choice)
+
+function getHumanChoice() {
+    let choice
+    switch(prompt("Please input your choice. 0 = rock 1 = paper 2 = scissors")){
+        case "0":
+            choice = "rock"
+            break
+        case "1":
+            choice = "paper"
+            break
+        case "2":
+            choice = "scissors"
+            break
+        default:
+            choice = "Please input the numbers indicated on the prompt"
+            break
+    }
+    return choice
+}
+
+const computerChoice = getComputerChoice()
+const humanChoice = getHumanChoice()
+console.log(computerChoice)
+console.log(humanChoice)
